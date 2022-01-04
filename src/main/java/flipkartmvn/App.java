0095933aa7972da103e6ac8ff9b076df00a58dcb
uday <udayrgpro@gmail.com>
2022-01-04 19:42:26 +0000

    new project generated from maven

---
+++ src/main/java/flipkartmvn/App.java
@@ -0,0 +1,13 @@
+package flipkartmvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
